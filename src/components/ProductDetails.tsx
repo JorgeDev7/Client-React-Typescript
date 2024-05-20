424d9316af3e5d17b7a57287ac01d62b209a7552
JorgeDev <jorgehumbertocolindres@gmail.com>
2024-05-20 10:52:39 -0600

    File: Action to delete product, creating his own form.
    Adding useFetcher for update availability

--- src/components/ProductDetails.tsx
+++ src/components/ProductDetails.tsx
@@ -1,16 +1,26 @@
-import { useNavigate } from "react-router-dom";
+import { Form, useNavigate, ActionFunctionArgs, redirect, useFetcher } from "react-router-dom";
 import { Product } from "../types";
 import { formatCurrency } from "../utils";
+import { deleteProduct } from "../services/ProductService";
 
 type ProductDetailsProps = {
     product: Product;
 };
+
+export async function action({ params }: ActionFunctionArgs) {
+
+    if (params.id !== undefined) {
+        await deleteProduct(+params.id);
+        return redirect('/');
+    }
+}
 
 export default function ProductDetails({ product }: ProductDetailsProps) {
 
     const { id, name, price, availability } = product;
     const isAvailable = availability;
 
+    const fetcher = useFetcher();
     const navigate = useNavigate();
 
     return (
@@ -22,7 +32,16 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
                 {formatCurrency(price)}
             </td>
             <td className="p-3 text-lg text-gray-800">
-                {isAvailable ? 'Disponible' : 'No Disponible'}
+                <fetcher.Form method="POST">
+                    <button
+                        type="submit"
+                        name="id"
+                        value={id}
+                        className={`${isAvailable ? 'text-black' : 'text-red-600'} rounded-lg p-2 text-xs uppercase font-bold w-full border border-black-100`}
+                    >
+                        {isAvailable ? 'Disponible' : 'No Disponible'}
+                    </button>
+                </fetcher.Form>
             </td>
             <td className="p-3 text-lg text-gray-800 ">
                 <div className="flex gap-2 items-center">
@@ -30,6 +49,23 @@ export default function ProductDetails({ product }: ProductDetailsProps) {
                         onClick={() => navigate(`productos/${id}/editar`)}
                         className="bg-indigo-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
                     >Editar</button>
+
+                    <Form
+                        className="w-full"
+                        method="POST"
+                        action={`productos/${id}/eliminar`}
+                        onSubmit={e => {
+                            if (!confirm(`¿Desea eliminar el producto: ${name}?`)) {
+                                e.preventDefault();
+                            }
+                        }}
+                    >
+                        <input
+                            type="submit"
+                            value="Eliminar"
+                            className="bg-red-600 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center cursor-pointer"
+                        />
+                    </Form>
                 </div>
             </td>
         </tr>
